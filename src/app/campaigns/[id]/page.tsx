15524d631af69e58dcfcb5aace3b734309e34a0b
ddri <davedri@gmail.com>
2025-02-15 14:56:51 +1000

    added codemod

--- src/app/campaigns/[id]/page.tsx
+++ src/app/campaigns/[id]/page.tsx
@@ -1,10 +1,12 @@
-'use client';
+'use client';;
+import { use } from "react";
 
 import { usePathname } from 'next/navigation';
 import CampaignPlanner from '@/components/CampaignPlanner';
 
-export default function CampaignPlannerPage({ params }: { params: { id: string } }) {
+export default function CampaignPlannerPage(props: { params: Promise<{ id: string }> }) {
+  const params = use(props.params);
   const pathname = usePathname();
-  
+
   return <CampaignPlanner campaignId={params.id} pathname={pathname} />;
 }
